226004da8e138657a1c8f66da316eeee44d413d1
ybcwork <supilya2020@gmail.com>
2022-08-26 19:11:20 +0300

    withdraw enterDeposit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 
 class Atm:
 
+    global balance
+    balance = 100
+
     @staticmethod
     def reg():
         global newLogin
@@ -20,8 +23,54 @@ class Atm:
         password = input()
         if newLogin == login and newPassword == password:
             print("You are logged into your account")
+            Atm.deposit()
         else:
             print("Wrong login or password")
+            Atm.auth()
+
+    @staticmethod
+    def enterDeposit():
+        global balance
+        print("Specify the amount you want to deposit: ")
+        countOfMoney = input()
+        if int(countOfMoney) >= 10000:
+            print("Mne kajetsa ti naebchik")
+            Atm.auth()
+        else:
+            balance += int(countOfMoney)
+            print("Money has been deposited")
+            Atm.deposit()
+
+    @staticmethod
+    def withdraw():
+        global balance
+        print("Specify the amount you want to withdraw: ")
+        withdraw = input()
+        if balance < int(withdraw):
+            print("There are not enough funds in your account")
+        else:
+            balance -= int(withdraw)
+            print("You have successfully withdrawn funds")
+        Atm.deposit()
+
+
+    @staticmethod
+    def deposit():
+        print('Choose option: \n'
+              'Press "1" to view balance \n'
+              'Press "2" to to enter deposit \n'
+              'Press "3" to withdraw')
+        option = input()
+        if option == "1":
+            print(f"You have {balance} $")
+            Atm.deposit()
+        if option == "2":
+            Atm.enterDeposit()
+        if option == "3":
+            Atm.withdraw()
+        else:
+            print("Incorrect number, you must choose 1, 2 or 3")
+            Atm.deposit()
 
     @staticmethod
     def menu():
@@ -36,7 +85,7 @@ class Atm:
             pass
         else:
             print("Incorrect number, you must choose 1 or 2")
-
+            Atm.menu()
 
 
 Atm.menu()
